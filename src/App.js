17ef7302a6076be230a5ac2c7ac1910b4e008f7f
orestisal97 <orestisal97@gmail.com>
2022-04-11 13:48:35 +0200

    testing-commit

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import MainContainer from "./components/MainContainer";
 import { VesselProvider } from "./context/vessel-context";
 
 function App() {
+
+    console.log("App");
+
   return (
     <div className="App">
       <VesselProvider>
